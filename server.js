2081645ff299c561a58d74a70a5c9bfd3d6243c0
Caleb Gill <calebbg@UNC-R90YDFXE.localdomain>
2022-09-01 08:54:26 -0400

    Adjusting webserver

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const http = require('http');
 const fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm).
-const args = require('minimist')(process.argv.slice(2));
+
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-
+const args = require('minimist')(process.argv.slice(2));
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args.name || 3000;
+const port = args.name || process.env.PORT || 3000;
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
@@ -40,7 +40,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end();
+  res.end(data);
 });
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
